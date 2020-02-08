f6673bb43f56e27c2477cf4d9f8677ce586a08c3
k0balt <funmarat@mail.ru>
2020-02-08 20:03:18 +0300

    remove uglify-js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,10 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 var webpack = require("webpack");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "#source-map",
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          sourceMap: true,
-          warnings: false
-        }
-      })
-    ]
-  },
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
